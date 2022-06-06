25f732b01e758b0e868572e8f02b9fe3e9b37281
lucas5203 <lucasdanielbrandan0@gmail.com>
2022-06-06 19:20:35 -0300

    JUEGO TERMINADO, implementacion de funcion mostrarInfo, jugar con varios pj y correcciones de tipeo

--- JuegoRPG/Program.cs
+++ JuegoRPG/Program.cs
@@ -4,6 +4,7 @@ namespace JuegoRPG
     class Program
     {
         static void Main(string[] args){
+            int seguirJugando = 0;
             string nombrePJ1, nombrePJ2;
             funciones funciones = new funciones();
 
@@ -27,64 +28,79 @@ namespace JuegoRPG
             jugadores.Add(PJ2);
 
             //MOSTRAMOS LOS DATOS DE LOS PERSONAJES
-            Console.WriteLine("\n******************************************************");
-            Console.WriteLine("***** Datos y Caracteristicas de los Personajes *****");
-            Console.WriteLine("******************************************************\n");
-            int idPj = 1;
-            foreach (var PJ in jugadores)
+            funciones.mostrarInformacion(jugadores);
+
+            do
             {
-                Console.WriteLine($"======== PJ{idPj} ({PJ.PjDatos.nombre}) ========");
-                Console.WriteLine("=======================================================");
-                Console.WriteLine("_DATOS: ");
-                funciones.mostrarDatosPJ(PJ);
-                Console.WriteLine("\n_CARACTERISTICAS: ");
-                funciones.mostrarCaracteristicasPJ(PJ);
-                Console.WriteLine("\n");
-                idPj++;
-            }
+                //PELEA
+                Console.WriteLine("\n******************************************************");
+                Console.WriteLine("***** FIGHT!! *****");
+                Console.WriteLine("******************************************************\n");
 
-            //PELEA
-            Console.WriteLine("\n******************************************************");
-            Console.WriteLine("***** FIGHT!! *****");
-            Console.WriteLine("******************************************************\n");
+                int rondas = 3, i;
+                for (i=0; i<rondas; i++)
+                {
+                    Console.WriteLine($"\n********************* RONDA {i+1} ******************************************");
+                    //ATAQUE Y DEFENSA
+                    Console.WriteLine($"===== ATACA {jugadores[0].PjDatos.nombre} ({jugadores[0].PjDatos.apodo}) | DEFIENDE {jugadores[1].PjDatos.nombre} ({jugadores[1].PjDatos.apodo}) =====");
+                    Console.WriteLine("============================================");
+                    funciones.mecanicaDeCombate(jugadores, 0, 1);
 
-            int rondas = 3, i;
-            for (i=0; i<rondas; i++)
-            {
-                Console.WriteLine($"\n********************* RONDA {i+1} *********************");
-                //ATAQUE Y DEFENSA
-                Console.WriteLine($"===== ATACA {jugadores[0].PjDatos.apodo} | DEFIENDE {jugadores[1].PjDatos.apodo} =====");
-                Console.WriteLine("============================================");
-                funciones.mecanicaDeCombate(jugadores, 0, 1);
+                    //CONTROL DE SALUD
+                    i = funciones.ganadorKO(jugadores, i); //Esta funcion controla el caso de que alguno gane sin haber terminado las 3 rondas, sino gano ninguno devuelve el valor actual de i, sino sale del for
 
-                Console.WriteLine($"\n===== ATACA {jugadores[1].PjDatos.apodo} | DEFIENDE {jugadores[0].PjDatos.apodo} =====");
-                Console.WriteLine("============================================");
-                funciones.mecanicaDeCombate(jugadores, 1, 0);
-                
-                //CONTROL DE SALUD
-                i = funciones.ganadorKO(jugadores, i); //Esta funcion controla el caso de que alguno gane sin haber terminado las 3 rondas, sino gano ninguno devuelve el valor actual de i, sino sale del for
-            }
-
-            //CONTROL DE SALUD
-            if(i < 5){
-                if(jugadores[0].PjDatos.salud < jugadores[1].PjDatos.salud){
-                    jugadores.RemoveAt(0);
-                }else{
-                    if(jugadores[1].PjDatos.salud < jugadores[0].PjDatos.salud){
-                        jugadores.RemoveAt(1);
+                    if(jugadores.Count > 1){ //En el caso de que todavia tenga vida el PJ2 entrara al if
+                        //ATAQUE Y DEFENSA
+                        Console.WriteLine($"\n===== ATACA {jugadores[1].PjDatos.nombre} ({jugadores[1].PjDatos.apodo}) | DEFIENDE {jugadores[0].PjDatos.nombre} ({jugadores[0].PjDatos.apodo}) =====");
+                        Console.WriteLine("============================================");
+                        funciones.mecanicaDeCombate(jugadores, 1, 0);
+                        
+                        //CONTROL DE SALUD
+                        i = funciones.ganadorKO(jugadores, i); //Esta funcion controla el caso de que alguno gane sin haber terminado las 3 rondas, sino gano ninguno devuelve el valor actual de i, sino sale del for
+                        Console.WriteLine("\n");
                     }
                 }
-            }
-            
-            if(jugadores.Count == 1){
-                Console.WriteLine("\n=========================================================");
-                Console.WriteLine($"_GANADOR: FELICIDADES {jugadores[0].PjDatos.nombre} ({jugadores[0].PjDatos.apodo})");
-                Console.WriteLine("=========================================================\n");
-            }else{
-                Console.WriteLine("\n=========================================================");
-                Console.WriteLine($"_EMPATE: {jugadores[0].PjDatos.nombre} ({jugadores[0].PjDatos.apodo}) Salud > {jugadores[0].PjDatos.salud} | {jugadores[1].PjDatos.nombre} ({jugadores[1].PjDatos.apodo}) Salud > {jugadores[1].PjDatos.salud}");
-                Console.WriteLine("=========================================================\n");
-            }
+
+                //CONTROL DE SALUD
+                if(i < 5){
+                    if(jugadores[0].PjDatos.salud < jugadores[1].PjDatos.salud){
+                        jugadores.RemoveAt(0);
+                    }else{
+                        if(jugadores[1].PjDatos.salud < jugadores[0].PjDatos.salud){
+                            jugadores.RemoveAt(1);
+                        }
+                    }
+                }
+                
+                //MOSTRAR GANADOR Y DAR BONUS
+                if(jugadores.Count == 1){
+                    Console.WriteLine("\n\n=========================================================");
+                    Console.WriteLine($"_GANADOR: FELICIDADES {jugadores[0].PjDatos.nombre} ({jugadores[0].PjDatos.apodo})");
+                    funciones.aumentoCaracteristicasDelGanador(jugadores);
+                    Console.WriteLine("=========================================================\n");
+                }else{
+                    Console.WriteLine("\n\n=========================================================");
+                    Console.WriteLine($"_EMPATE: {jugadores[0].PjDatos.nombre} ({jugadores[0].PjDatos.apodo}) Salud > {jugadores[0].PjDatos.salud} | {jugadores[1].PjDatos.nombre} ({jugadores[1].PjDatos.apodo}) Salud > {jugadores[1].PjDatos.salud}");
+                    Console.WriteLine("=========================================================\n");
+                }
+
+                //DESEA SEGUIR JUGANDO?
+                Console.WriteLine("Desea seguir jugando? (Si=1 | No=0)");
+                seguirJugando = Convert.ToInt32(Console.ReadLine());
+
+                if(seguirJugando == 1){
+                    Console.WriteLine("\n_Ingrese el nombre del PJ nuevo: ");
+                    string nombrePJNuevo = Console.ReadLine();
+                    Personaje PJNuevo = funciones.crearPJ(nombrePJNuevo);
+                    jugadores.Add(PJNuevo);
+                    funciones.mostrarInformacion(jugadores);
+                }
+
+            PAPA while (seguirJugando == 1);
+
+            Console.WriteLine("\n\n=========================================================");
+            Console.WriteLine("============= GAME OVER =============");
+            Console.WriteLine("=========================================================\n");
         }
     }
 }
--- JuegoRPG/funciones.cs
+++ JuegoRPG/funciones.cs
@@ -10,6 +10,23 @@ namespace JuegoRPG
             Personaje PJ = new Personaje(caracteristicasPJ, datosPJ);
             return PJ;
         }
+        public void mostrarInformacion(List<Personaje> PJS){
+            Console.WriteLine("\n******************************************************");
+            Console.WriteLine("***** Datos y Caracteristicas de los Personajes *****");
+            Console.WriteLine("******************************************************\n");
+            int idPj = 1;
+            foreach (var PJ in PJS)
+            {
+                Console.WriteLine($"======== PJ{idPj} ({PJ.PjDatos.nombre}) ========");
+                Console.WriteLine("=======================================================");
+                Console.WriteLine("_DATOS: ");
+                this.mostrarDatosPJ(PJ);
+                Console.WriteLine("\n_CARACTERISTICAS: ");
+                this.mostrarCaracteristicasPJ(PJ);
+                Console.WriteLine("\n");
+                idPj++;
+            }
+        }
         public void mostrarDatosPJ(Personaje _PJ){
             Console.WriteLine("Nombre: "+_PJ.PjDatos.nombre);
             Console.WriteLine("Apodo: "+_PJ.PjDatos.apodo);
@@ -19,11 +36,11 @@ namespace JuegoRPG
             Console.WriteLine("Salud: "+_PJ.PjDatos.salud);
         }
         public void mostrarCaracteristicasPJ(Personaje _PJ){
-            Console.WriteLine("Velocidad: "+_PJ.PjCaracteristicas.velocidad);
-            Console.WriteLine("Destreza: "+_PJ.PjCaracteristicas.destreza);
-            Console.WriteLine("Fuerza: "+_PJ.PjCaracteristicas.fuerza);
-            Console.WriteLine("Nivel: "+_PJ.PjCaracteristicas.nivel);
-            Console.WriteLine("Armadura: "+_PJ.PjCaracteristicas.armadura);
+            Console.WriteLine("Velocidad: "+Math.Round(_PJ.PjCaracteristicas.velocidad, 2));
+            Console.WriteLine("Destreza: "+Math.Round(_PJ.PjCaracteristicas.destreza, 2));
+            Console.WriteLine("Fuerza: "+Math.Round(_PJ.PjCaracteristicas.fuerza, 2));
+            Console.WriteLine("Nivel: "+Math.Round(_PJ.PjCaracteristicas.nivel, 2));
+            Console.WriteLine("Armadura: "+Math.Round(_PJ.PjCaracteristicas.armadura, 2));
         }
         public void mecanicaDeCombate(List<Personaje> _jugadores, int _pjAtaque, int _pjDefensa){
             Random nRand = new Random();
@@ -56,7 +73,48 @@ namespace JuegoRPG
             return i; //sino entra en ninguna condicion devolvera el valor actual de i para que siga controlando
         }
         public void aumentoCaracteristicasDelGanador(List<Personaje> _ganador){
-            
+            Random nRand = new Random();
+            int caractRandom = nRand.Next(1, 7);
+            int cincoAdiez = nRand.Next(5, 11);
+            int dosAseis = nRand.Next(2, 7);
+
+            switch (caractRandom)
+            {
+                case 1:
+                    if(_ganador[0].PjCaracteristicas.armadura < 10){
+                        _ganador[0].PjCaracteristicas.armadura += (_ganador[0].PjCaracteristicas.armadura*cincoAdiez)/100; //suma entre un 5% a 10%
+                        Console.WriteLine($"-Se incremento un %{cincoAdiez} de Armadura.");
+                    }else{Console.WriteLine("-No se incremento, nivel maximo de Armadura.");}
+                    break;
+                case 2:
+                    if(_ganador[0].PjCaracteristicas.destreza < 5){
+                        _ganador[0].PjCaracteristicas.destreza += (_ganador[0].PjCaracteristicas.destreza*dosAseis)/100; //suma entre un 2% y 6%
+                        Console.WriteLine($"-Se incremento un %{dosAseis} de Destreza.");
+                    }else{Console.WriteLine("-No se incremento, nivel maximo de Destreza.");}
+                    break;
+                case 3:
+                    if(_ganador[0].PjCaracteristicas.fuerza < 10){
+                        _ganador[0].PjCaracteristicas.fuerza += (_ganador[0].PjCaracteristicas.fuerza*cincoAdiez)/100; //suma entre un 5% a 10%
+                        Console.WriteLine($"-Se incremento un %{cincoAdiez} de Fuerza.");
+                    }else{Console.WriteLine("-No se incremento, nivel maximo de Fuerza.");}
+                    break;
+                case 4:
+                    if(_ganador[0].PjCaracteristicas.nivel < 10){
+                        _ganador[0].PjCaracteristicas.nivel += 1;
+                        Console.WriteLine($"-Se incremento 1 Nivel."); //sumamos 1 nivel
+                    }else{Console.WriteLine("-No se incremento el Nivel, nivel maximo.");}
+                    break;
+                case 5:
+                    if(_ganador[0].PjCaracteristicas.velocidad <10){
+                        _ganador[0].PjCaracteristicas.velocidad += (_ganador[0].PjCaracteristicas.velocidad*cincoAdiez)/100; //suma entre un 5% a 10%
+                        Console.WriteLine($"-Se incremento un %{cincoAdiez} de Velocidad.");
+                    }else{Console.WriteLine("-No se incremento, nivel maximo de Velocidad.");}
+                    break;
+                default:
+                    _ganador[0].PjDatos.salud += 10;
+                    Console.WriteLine($"-Se incremento 10 puntos de Salud.");
+                    break;
+            }
         }
     }
 }
